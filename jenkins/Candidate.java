74fb4582226fb6ee83426c8ffee08e56e7e9cfed
git-shru <mca.1904@unigoa.ac.in>
2021-06-29 16:02:38 +0530

    add code

---
+++ jenkins/Candidate.java
@@ -0,0 +1,57 @@
+package jenkins;
+
+public class Candidate {
+	
+	private int id;
+	private String name;
+	private int academicsPoints;
+	private int extraPoints;
+	
+	
+	
+	public Candidate(int id, String name, int academicsPoints, int extraPoints) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.academicsPoints = academicsPoints;
+		this.extraPoints = extraPoints;
+	}
+
+	public void calculatePoints() {
+		
+		int totalPoints = academicsPoints + extraPoints;
+		System.out.println("total points :" + totalPoints);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAcademicsPoints() {
+		return academicsPoints;
+	}
+
+	public void setAcademicsPoints(int academicsPoints) {
+		this.academicsPoints = academicsPoints;
+	}
+
+	public int getExtraPoints() {
+		return extraPoints;
+	}
+
+	public void setExtraPoints(int extraPoints) {
+		this.extraPoints = extraPoints;
+	}
+	
+	
+
+}
---
+++ jenkins/Main.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Candidate candidate = new Candidate(123,"candidate1",3,3);
+		
+		candidate.calculatePoints();
+
+	}
+
+}
